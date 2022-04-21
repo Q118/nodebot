56d535978a78ad61853c18a263b69a6fda2f9913
Shelby Rothman <shelby.rothman@aderant.com>
2022-04-21 15:20:24 -0700

    stopping point, return to find responseOptons are undefined

--- routes/bots/slack.js
+++ routes/bots/slack.js
@@ -1,28 +1,48 @@
 const express = require('express');
 const { createEventAdapter } = require('@slack/events-api');
 const { WebClient } = require('@slack/web-api');
-
+const moment = require('moment');
 
 const router = express.Router();
 
 module.exports = (params) => {
-    const { config } = params; // destructuring.. config will be the config property of the params object
+    const { config, witService, reservationService } = params;
 
     const slackEvents = createEventAdapter(config.slack.signingSecret);
     const slackWebClient = new WebClient(config.slack.token);
 
-    router.use('/events', slackEvents.requestListener()); //? this event-middleware looks for registered event handlers that will be executed when an event occurs.
-    //then we register it with 'on' method.
+    router.use('/events', slackEvents.requestListener());
 
-    const handleMention = async (event) => {
+    async function handleMention(event) {
+        const mention = /<@[A-Z0-9]+>/;
+        const eventText = event.text.replace(mention, '').trim();
+
+        let text = '';
+
+        if (!eventText) {
+            text = 'Hey!';
+        } else {
+            const entities = await witService.query(eventText);
+            const { intent, customerName, reservationDateTime, numberOfGuests } = entities;
+
+            if (!intent || intent !== 'reservation' || !customerName || !reservationDateTime || !numberOfGuests) {
+                text = 'Sorry - could you rephrase that?';
+                console.log(entities);
+            } else {
+                const reservationResult = await reservationService
+                    .tryReservation(moment(reservationDateTime).unix(), numberOfGuests, customerName);
+                text = reservationResult.success || reservationResult.error;
+            }
+        }
+
         return slackWebClient.chat.postMessage({
-            text: `Hi there, I\'m Resi. What can I do for you?`,
+            text,
             channel: event.channel,
             username: 'Resi',
         });
     }
 
     slackEvents.on('app_mention', handleMention);
-    
+
     return router;
-}
+};
