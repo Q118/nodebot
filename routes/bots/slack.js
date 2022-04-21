ba70a66e8ba8536cac4c3ace8a4b72df9a7bc37c
Shelby Rothman <shelby.rothman@aderant.com>
2022-04-21 09:27:48 -0700

    resi responding in chat

--- routes/bots/slack.js
+++ routes/bots/slack.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
+
 
 const router = express.Router();
 
@@ -8,13 +9,20 @@ module.exports = (params) => {
     const { config } = params; // destructuring.. config will be the config property of the params object
 
     const slackEvents = createEventAdapter(config.slack.signingSecret);
+    const slackWebClient = new WebClient(config.slack.token);
 
     router.use('/events', slackEvents.requestListener()); //? this event-middleware looks for registered event handlers that will be executed when an event occurs.
     //then we register it with 'on' method.
 
-    slackEvents.on('app_mention', (event) => {
-        console.log(`Received mention event: ${JSON.stringify(event)}`);
-    })
+    const handleMention = async (event) => {
+        return slackWebClient.chat.postMessage({
+            text: `Hi there, I\'m Resi. What can I do for you?`,
+            channel: event.channel,
+            username: 'Resi',
+        });
+    }
 
+    slackEvents.on('app_mention', handleMention);
+    
     return router;
 }
