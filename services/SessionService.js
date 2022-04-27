4e93893567b46f1bcd51d6b2ee6f61295e43bb8e
Shelby Rothman <shelby.rothman@aderant.com>
2022-04-27 09:47:51 -0700

    initialized sessionService

--- services/SessionService.js
+++ services/SessionService.js
@@ -6,7 +6,6 @@ class SessionService {
     static now() {
         return Math.floor(new Date() / 1000) //unix timestamp in seconds
     }
-
     create(sessionId) {
         this.cleanup();
         // provide this sessionId that uniquely identifies the session from the outset bc it is dependent on the front end
@@ -22,20 +21,17 @@ class SessionService {
         this.update(sessionId);
         return this.sessions[sessionId];
     }
-
     delete(sessionId) {
         if (!this.sessions[sessionId]) return false;
         delete this.sessions[sessionId];
         return true;
     }
-
     update(sessionId) {
         this.cleanup();
         if (!this.sessions[sessionId]) return false;
         this.sessions[sessionId].timestamp = SessionService.now();
         return this.sessions[sessionId];
     }
-
     cleanup() { //cleans up expired sessions
         const now = SessionService.now();
         Object.keys(this.sessions).forEach((key) => {
